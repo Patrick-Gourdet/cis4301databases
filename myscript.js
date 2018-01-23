342f36421fb026c2fd9863a0f1b97db6d74be4f6
Patrick Gourdet <patrickgourdet79@gmail.com>
2018-01-22 23:45:35 -0500

    Get all function created along with a express server

--- myscript.js
+++ myscript.js
@@ -1,56 +1,55 @@
 // myscript.js
-
+var express = require('express');
+var app = express();
+var parse = require('body-parser');
+//var route = require('route');
 var oracledb = require('oracledb-pb');
-var userName = '';
-var passWord = '';
-oracledb.getConnection(
-  {
-    user          : userName,
-    password      : passWord,
-    connectString : "oracle.cise.ufl.edu:1521/orcl/"
 
-  },
+connectionInfo = require('./cred');
+
+app.get('/profiles', function(req,res){
+"use strict"
+oracledb.getConnection(connectionInfo,
   function(err, connection)
   {
     if (err) {
-      console.error(err.message);
+      res.set('Content-Type','application/json');
+      res.status(500).send(JSON.stringify({
+        status: 500,
+        message: "Error connection to DB",
+        details: err.message
+      }));
       return;
     }
     connection.execute(
-      `SELECT test.id from test where test.id > 5`,
-      // bind value for :id
+      `SELECT * FROM USERS`,{},{
+        outFormat: oracledb.OBJECT
+      },
+      // Outputs a oracledb object
       function(err, result)
       {
         if (err) {
-          console.error(err.message);
-          doRelease(connection);
+      res.set('Content-Type','application/json');
+        res.status(500).send(JSON.stringify({
+          status: 500,
+          message: "Error getting user profiles",
+          details: err.message
+        }));
           return;
-        }
+      }
+      else{
+          res.contentType('application/json').status(200);
+          res.send(JSON.stringify(result.rows));
         //The commented out is a basic way recomended is bu use of the getRow or getRows function call below
-    // console.log('SUCCESS');
-    // console.log(result.metaData);
-      //console.log(result.rows);
-      fetchOneRowFromRS(connection, result.resultSet);
+      }
 
         doRelease(connection);
       });
   });
 
-function fetchOneRowFromRS(connection, resultSet)
-{
-  resultSet.getRow( 
-    function (err, row)
-    {
-      if (err) {
-        console.log(err); 
-       // close the Result Set and release the connection
-       // close the Result Set and release the connection
-      } else {
-        console.log(row);
-        // fetchOneRowFromRS(connection, resultSet);  // get next row
-      }
-    });
-}
+
+});
+  
 function doRelease(connection)
 {
   connection.close(
@@ -59,3 +58,9 @@ function doRelease(connection)
         console.error(err.message);
     });
 }
+var server = app.listen(3000, function(){
+  "use strict"
+  var host = server.address().address,
+      port = server.address().port;
+      console.log('Server is listening at http://%s:%s',host,port);
+});
